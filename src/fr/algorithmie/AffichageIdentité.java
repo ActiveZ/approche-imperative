188c6d15471bc1264e9e0ea13385a31f7dc66067
ActiveZ <activez.git@gmail.com>
2021-12-09 18:30:03 +0100

    fin tp affichage identite

---
+++ src/fr/algorithmie/AffichageIdentité.java
@@ -0,0 +1,18 @@
+/**
+ * Diginamic TP 03
+ * 9/12/2021
+ * openjdk 17.0.1
+ * Arnaud Couturier
+ */
+
+package fr.algorithmie;
+
+public class AffichageIdentité {
+    public static void main(String[] args) {
+
+        // Boucle for pour afficher 10 fois son identité
+        for (int i = 0; i < 10; i++) {
+            System.out.println("je m'appelle Arnaud Couturier");
+        }
+    }
+}
