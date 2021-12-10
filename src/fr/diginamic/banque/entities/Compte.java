3fc1ac43502b554d6ee7f28c859aa72ff25b1de9
ActiveZ <activez.git@gmail.com>
2021-12-10 17:07:25 +0100

    fin tp 6

--- src/fr/diginamic/banque/entities/Compte.java
+++ src/fr/diginamic/banque/entities/Compte.java
@@ -8,4 +8,13 @@ public class Compte {
         accountNumber = numero;
         accountValue = solde;
     }
+
+    // surchage de la fonction toString
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", accountValue=" + accountValue +
+                '}';
+    }
 }
