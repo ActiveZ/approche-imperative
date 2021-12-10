a01e0d50a3a698d220f8e6ca933d6e2edfd48f57
ActiveZ <activez.git@gmail.com>
2021-12-10 14:06:23 +0100

    fin tp 1

--- src/fr/algorithmie/Interactif21Batons.java
+++ src/fr/algorithmie/Interactif21Batons.java
@@ -29,14 +29,39 @@ import java.util.Scanner;
 
 public class Interactif21Batons {
     public static void main(String[] args) {
+        boolean player; // joueur en cours: false -> pc, true -> humain
+        String s; // valeur à tester entrée dans le scanner
+        int nbBar = 21;
 
-        // sélection d'un nombre entre 1 et 3
         Scanner sc = new Scanner(System.in);
-        String s; // valeur à tester
+        // définir qui commence la partie
         do {
-            System.out.println("Entrez un nombre entre 1 et 100");
+            System.out.println("Qui commence ? 0: ordinateur, 1: vous");
             s = sc.nextLine();
-        } while (!s.matches("[1-3]"));
+        } while (!s.matches("[0-1]"));
+        player = Boolean.parseBoolean(s);
+
+        // jeu jusqu'à ce qu'il ne reste qu'un baton
+        do {
+            System.out.println("Il reste " + nbBar + " batons");
+            // le joueur joue
+            if (player) {
+                // sélection d'un nombre entre 1 et 3 pour enlever les batons
+                do {
+                    System.out.println("Enlevez entre 1 et 3 batons:");
+                    s = sc.nextLine();
+                } while (!s.matches("[1-3]"));
+                nbBar =- Integer.parseInt(s);
+                player = !player;
+            }
+            // l'ordinateur joue
+            else {
+                System.out.println("L'ordinateur joue et enlève batons");
+                player = !player;
+            }
+        } while (nbBar>1);
+
+
     }
 }
 
