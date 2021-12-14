0bfe77ef2e89e3e9bbb98725a49a2942288a8f2e
ActiveZ <activez.git@gmail.com>
2021-12-14 14:03:24 +0100

    tp 10 - exercice 1 correction interface arraylist -> list

--- src/fr/listes/CreationListe.java
+++ src/fr/listes/CreationListe.java
@@ -1,12 +1,13 @@
 package fr.listes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CreationListe {
     public static void main(String[] args) {
 
         // arraylist d'entiers de 1 à 100
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 1; i < 101; i++) {
             list.add(i);
         }
